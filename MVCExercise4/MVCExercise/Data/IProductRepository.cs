3aadddfcdcc0d9a3c603d8fc0ca9521eb48ffe7f
JayHunds <159581623+JayHunds@users.noreply.github.com>
2024-04-01 07:46:42 -0700

    Completed exercise 4

---
+++ MVCExercise4/MVCExercise/Data/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using MVCExercise.Models;
+
+namespace MVCExercise.Data
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        void InsertProduct(Product productToInsert);
+        IEnumerable<Category> GetCategories();
+        Product AssignCategory();
+    }
+}
